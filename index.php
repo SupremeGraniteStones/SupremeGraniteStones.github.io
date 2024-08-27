30b8d90c5bb9a7184b9e5fbb75b9f1a02de9f37b
vivek7755 <vivekvicku7755@gmail.com>
2024-08-27 15:46:15 +0530

    gscupdate4

--- index.php
+++ index.php
@@ -196,11 +196,15 @@
         }
     </style>
 
+    <!-- Load Non-Critical CSS Asynchronously -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" media="print" onload="this.media='all'">
     <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"></noscript>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" media="print" onload="this.media='all'">
     <noscript><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"></noscript>
+
+    <link rel="stylesheet" href="newstyles.css" media="print" onload="this.media='all'">
+    <noscript><link rel="stylesheet" href="newstyles.css"></noscript>
 
     <!-- Structured Data -->
     <script type="application/ld+json">
@@ -536,7 +540,7 @@
     </footer>
 
     <!-- JavaScript -->
-    <script nonce="<GENERATED_NONCE>">
+    <script>
         // Active nav link
         const navLinks = document.querySelectorAll('.nav-link');
         navLinks.forEach(link => {
